9c1be7a6ce68e28852e9debdeb0267b61efb60af
A350Fan <A350Fan@web.de>
2025-06-28 19:10:06 +0200

    An Optimization

--- app/src/main/java/com/example/schulmanager/MainActivity.java
+++ app/src/main/java/com/example/schulmanager/MainActivity.java
@@ -27,12 +27,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Statusleiste transparent machen
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         // Ende transparente Statusleiste
         setContentView(R.layout.activity_main);
 

6eebedee1ab7de35b5bc31f292e326803016f851
A350Fan <A350Fan@web.de>
2025-06-28 18:58:02 +0200

    Moved the Subject Boxes down so that they don't sink under the black box "Notenmanager"

--- app/src/main/java/com/example/schulmanager/MainActivity.java
+++ app/src/main/java/com/example/schulmanager/MainActivity.java
@@ -10,7 +10,10 @@ import com.example.schulmanager.fragments.NotenmanagerFragment;
 import com.example.schulmanager.fragments.StundenplanFragment;
 import com.example.schulmanager.fragments.PruefungenFragment;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
@@ -23,6 +26,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Statusleiste transparent machen
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
         setContentView(R.layout.activity_main);
 
         ViewPager2 viewPager = findViewById(R.id.view_pager);
